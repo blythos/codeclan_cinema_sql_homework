73ede4b49b67f0ae813bdff0c53578c14ed68238
Stephen Blythe <stephenblythe@gmail.com>
2019-08-02 13:30:04 +0100

    Sets up basic Film class, and adds a film object to console.rb

--- console.rb
+++ console.rb
@@ -1,2 +1,9 @@
 require('pry-byebug')
 require_relative('models/Film.rb')
+
+film1 = Film.new(
+  {
+    'title' => 'The Room',
+    'price' => 10
+  }
+)
--- models/Film.rb
+++ models/Film.rb
@@ -1,5 +1,9 @@
 class Film
 
-
+  def initialize(film)
+    @id = film['id'].to_i if film['id']
+    @title = film['title']
+    @price = film['price'].to_i
+  end
 
 end
