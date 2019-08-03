b79fa96351312e6ceaf898135c63f71fc20b2f6e
Stephen Blythe <stephenblythe@gmail.com>
2019-08-03 11:42:11 +0100

    Refactors the Customer.films() method to function after addition of new table

--- models/Customer.rb
+++ models/Customer.rb
@@ -50,10 +50,8 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
-
-
   def films()
-    sql = "SELECT films.* FROM films INNER JOIN tickets ON films.id = tickets.film_id WHERE tickets.customer_id = $1"
+    sql = "SELECT films.* FROM films INNER JOIN screenings ON films.id = screenings.film_id INNER JOIN tickets ON screenings.id = tickets.screening_id WHERE tickets.customer_id = $1"
     values = [@id]
     films = SqlRunner.run(sql, values)
     return films.map { |film| Film.new(film) }
